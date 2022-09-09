c56c0dd83267450e8d1f97dc4a7ffa87bd3211ad
hee.youn <heesung6701@naver.com>
2022-09-09 20:21:04 +0900

    perfumeImage update script 버그 수정

--- rawfile/src/scripts/updatePerfumeImageUrl.py
+++ rawfile/src/scripts/updatePerfumeImageUrl.py
@@ -1,5 +1,5 @@
 from api.src.internal.entity.PerfumeEntity import PerfumeEntity
-from api.src.internal.sql.SqlModel import SqlModel
+from api.src.internal.sql.SqlModel import perfume_model
 from api.src.internal.sql.SqlUtil import SQLUtil
 
 
@@ -7,9 +7,11 @@ def main(perfume_idx_list):
     fail_list = []
     for perfume_idx in perfume_idx_list:
         image_url = "https://afume-release.s3.ap-northeast-2.amazonaws.com/perfumes/{}_1.png".format(perfume_idx)
-        perfume = PerfumeEntity(perfume_idx=perfume_idx, name=None, english_name=None, story=None, volume_and_price=None,
+        perfume = PerfumeEntity(perfume_idx=perfume_idx, name=None, english_name=None, story=None,
+                                volume_and_price=None,
                                 abundance_rate=None, image_url=image_url)
-        affected_rows = SqlModel.instance().update(perfume)
+        print(perfume.__dict__)
+        affected_rows = perfume_model.update(perfume.__dict__)
         if affected_rows == 0:
             fail_list.append(perfume_idx)
     print("faile list : {}".format(fail_list))
