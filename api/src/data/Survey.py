3b7e2ffcfb15cfb8c5296c3fcd1ea3dc80d13706
hee.youn <heesung6701@gmail.com>
2022-12-20 13:33:50 +0900

    Add missing Survey class

---
+++ api/src/data/Survey.py
@@ -0,0 +1,9 @@
+class Survey:
+
+    def __init__(self, _id: int, user_idx: str, keyword_list: [int], perfume_list: [int], series_list: [int]):
+        self._id = _id
+        self.user_idx = user_idx
+        self.keyword_list = keyword_list
+        self.perfume_list = perfume_list
+        self.series_list = series_list
+
