a236a49fa1a950087e636f08158da4e46340c1f0
hee.youn <heesung6701@gmail.com>
2022-12-20 13:30:50 +0900

    add get_user_info_list and get_survey_info_list

--- analysis/src/repository/__init__.py
+++ analysis/src/repository/__init__.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
 from api.src.service import PerfumeService
+from api.src.service import UserService
 
 
 def get_perfume_ingredient_info_list() -> pd.DataFrame:
     return PerfumeService.get_perfume_ingredient_info_list()
+
+
+def get_user_info_list() -> pd.DataFrame:
+    return UserService.get_user_info_list()
+
+
+def get_survey_info_list() -> pd.DataFrame:
+    return UserService.get_user_survey_info_list()
 
 
 def __main():
--- api/src/service/__init__.py
+++ api/src/service/__init__.py
@@ -4,6 +4,9 @@ import pandas as pd
 from api.src.common.Object import Singleton
 from api.src.internal.entity.PerfumeEntity import PerfumeEntity
 from api.src.internal.sql.SqlUtil import SQLUtil
+from api.src.repository.KeywordRepository import KeywordRepository
+from api.src.repository.SeriesRepository import SeriesRepository
+from api.src.repository.SurveyRepository import SurveyRepository
 
 COL_PERFUME_IDX = 'perfume_idx'
 COL_PERFUME_NAME = 'perfume_name'
@@ -17,6 +20,26 @@ COL_BRAND_ENGLISH_NAME = 'brand_english_name'
 COL_INGREDIENT_NAME_LIST = 'ingredient_name_list'
 COL_CATEGORY_NAME_LIST = 'category_name_list'
 COL_SERIES_NAME_LIST = 'series_name_list'
+
+COL_USER_IDX = 'user_idx'
+COL_USER_NICKNAME = 'nickname'
+COL_USER_EMAIL = 'email'
+COL_USER_GENDER = 'gender'
+COL_USER_BIRTH = 'birth_year'
+COL_CREATED_AT = 'created_at'
+COL_ACCESS_TIME = 'access_time'
+COL_USER_LIKE_PERFUME_LIST = 'liked_perfume_idx_list'
+COL_USER_REVIEWED_PERFUME_LIST = 'reviewed_perfume_list'
+
+COL_SURVEY_PERFUME_LIST = 'survey_perfume_idx_list'
+COL_SURVEY_SERIES_LIST = 'survey_series_list'
+COL_SURVEY_KEYWORD_LIST = 'survey_keyword_list'
+
+
+def convert_to_array(text: str) -> str:
+    if text is None:
+        return ""
+    return str(sorted(text.split(',')))
 
 
 class PerfumeService(Singleton):
@@ -57,11 +80,6 @@ class PerfumeService(Singleton):
                                                COL_CATEGORY_NAME_LIST, COL_SERIES_NAME_LIST)
         result = sql_util.execute(sql=sql)
 
-        def convert_to_array(text: str) -> str:
-            if text is None:
-                return ""
-            return str(sorted(text.split(',')))
-
         dummy = np.array(
             [
                 [
@@ -89,25 +107,120 @@ class PerfumeService(Singleton):
         return df
 
 
+class UserService(Singleton):
+
+    # 회원 정보
+    # - 하트 누른 향수: liked_perfume_idx_list [perfume_idx, ...]
+    # - 향수 별점 준 현황: reviewed_perfume_list e.g) [perfume_idx:score,...]
+    @staticmethod
+    def get_user_info_list() -> pd.DataFrame:
+        sql_util = SQLUtil.instance()
+        sql = "SELECT u.user_idx as '{}', u.nickname as '{}', " \
+              "u.email as '{}', " \
+              "u.gender as '{}', " \
+              "u.birth as '{}', " \
+              "u.created_at as '{}', " \
+              "u.access_time as '{}', " \
+              "GROUP_CONCAT(DISTINCT(lp.perfume_idx)) AS '{}', " \
+              "GROUP_CONCAT(DISTINCT(r.perfume_idx), \":\", r.score) AS '{}' " \
+              "FROM users u " \
+              "LEFT JOIN like_perfumes AS lp " \
+              "ON u.user_idx = lp.user_idx " \
+              "LEFT JOIN reviews AS r " \
+              "ON u.user_idx = r.user_idx " \
+              "WHERE u.deleted_at IS NULL " \
+              "AND r.deleted_at IS NULL " \
+              "GROUP BY u.user_idx ".format(COL_USER_IDX, COL_USER_NICKNAME, COL_USER_EMAIL,
+                                            COL_USER_GENDER, COL_USER_BIRTH,
+                                            COL_CREATED_AT, COL_ACCESS_TIME,
+                                            COL_USER_LIKE_PERFUME_LIST,
+                                            COL_USER_REVIEWED_PERFUME_LIST)
+        result = sql_util.execute(sql=sql)
+        datas = np.array(
+            [
+                [
+                    item[COL_USER_IDX],
+                    item[COL_USER_NICKNAME],
+                    item[COL_USER_EMAIL],
+                    item[COL_USER_GENDER],
+                    item[COL_USER_BIRTH],
+                    item[COL_CREATED_AT].isoformat(),
+                    item[COL_ACCESS_TIME].isoformat(),
+                    convert_to_array(item[COL_USER_LIKE_PERFUME_LIST]),
+                    convert_to_array(item[COL_USER_REVIEWED_PERFUME_LIST])
+                ] for item in result
+            ])
+        index = list(map(lambda it: it[COL_USER_IDX], result))
+        df = pd.DataFrame(data=datas, index=index, columns=[
+            COL_USER_IDX, COL_USER_NICKNAME, COL_USER_EMAIL, COL_USER_GENDER,
+            COL_USER_BIRTH, COL_CREATED_AT,
+            COL_ACCESS_TIME, COL_USER_LIKE_PERFUME_LIST, COL_USER_REVIEWED_PERFUME_LIST
+        ])
+        return df
+
+
+    @staticmethod
+    def get_user_survey_info_list() -> pd.DataFrame:
+        surveys = SurveyRepository.get_survey_all()
+
+        datas = np.array(
+            [
+                [
+                    item.user_idx,
+                    str(item.perfume_list),
+                    SeriesRepository.get_series_by_idx_list(item.series_list),
+                    KeywordRepository.get_keywords_by_idx_list(item.keyword_list),
+                ] for item in surveys
+            ])
+        index = list(map(lambda it: it.user_idx, surveys))
+        df = pd.DataFrame(data=datas, index=index, columns=[
+            COL_USER_IDX, COL_SURVEY_PERFUME_LIST, COL_SURVEY_SERIES_LIST, COL_SURVEY_KEYWORD_LIST
+        ])
+        return df
+
+
 def __main():
-    return PerfumeService.instance().get_perfume_ingredient_info_list()
+    # return PerfumeService.instance().get_perfume_ingredient_info_list()
+    return UserService.instance().get_user_info_list()
+
+
+def testPerfumeInfoList():
+    res = PerfumeService.instance().get_perfume_ingredient_info_list()
+
+    file_nm = "temp.xlsx"
+
+    res.to_excel("./{}".format(file_nm),
+                 sheet_name='temp',
+                 na_rep='',
+                 float_format="%.2f",
+                 header=True,
+                 index=False,
+                 index_label="id",
+                 startrow=1,
+                 startcol=1,
+                 # engine = 'xlsxwriter',
+                 freeze_panes=(2, 0)
+                 )
+
+
+def testUserInfoList():
+    res = UserService.instance().get_user_survey_info_list()
+
+    file_nm = "temp.xlsx"
+
+    res.to_excel("./{}".format(file_nm),
+                 sheet_name='temp',
+                 na_rep='',
+                 float_format="%.2f",
+                 header=True,
+                 index=False,
+                 index_label="user_idx",
+                 startrow=1,
+                 startcol=1,
+                 # engine = 'xlsxwriter',
+                 freeze_panes=(2, 0)
+                 )
 
 
 if __name__ == '__main__':
-    df = __main()
-
-    # file_nm = "temp.xlsx"
-
-    # df.to_excel("./{}".format(file_nm),
-    #             sheet_name='temp',
-    #             na_rep='',
-    #             float_format="%.2f",
-    #             header=True,
-    #             index=False,
-    #             index_label="id",
-    #             startrow=1,
-    #             startcol=1,
-    #             # engine = 'xlsxwriter',
-    #             freeze_panes=(2, 0)
-    #             )
-    print(df)
+    testUserInfoList()
